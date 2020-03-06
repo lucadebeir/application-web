770835450f7e144dc571eba82b1b8deb4f73c485
marineteroitin <marine.teroitin@gmail.com>
2020-03-06 08:26:22 +0100

    test upload image ok

--- serveur.js
+++ serveur.js
@@ -3,11 +3,16 @@ var cors = require("cors")
 var bodyParser = require("body-parser")
 var app = express()
 var port = process.env.PORT || 3000
-var sanitizer  = require("express-sanitizer") //pour proteger les injection 
+var sanitizer = require("express-sanitizer") //pour proteger les injection 
 const date = require('date-and-time')
-const multer = require('multer')
-var multerGoogleStorage = require('multer-google-storage');
+let methodOverride = require('method-override');
+let fileUpload = require('express-fileupload');
+const { Storage } = require('@google-cloud/storage');
+let multer = require('multer');
+let multerGoogleStorage = require('multer-google-storage');
 const path = require('path');
+const uploadImage = require('./helpers/helpers')
+
 
 app.use(sanitizer())
 app.use(bodyParser.json())
@@ -26,14 +31,37 @@ app.listen(port, function () {
     console.log("Server is running on port " + port)
 })
 
+const multerMid = multer({
+    storage: multer.memoryStorage(),
+    limits: {
+        fileSize: 5 * 1024 * 1024,
+    },
+})
+app.disable('x-powered-by')
+app.use(multerMid.single('file'))
+
+app.post('/uploads', async (req, res, next) => {
+    try {
+      const myFile = req.file
+      console.log(req)
+      const imageUrl = await uploadImage(myFile)
+  
+      res
+        .status(200)
+        .json({
+          message: "Upload was successful",
+          data: imageUrl
+        })
+    } catch (error) {
+      next(error)
+    }
+  })
+
+
+app.use(methodOverride('_method'));
+app.use(fileUpload());
+
 //pour google cloud
-var uploadHandler = multer({
-    storage: multerGoogleStorage.storageEngine({
-        keyFilename: "./molten-calling-270223-7d6d4b3665fa.json",
-        projectId: 'molten-calling-270223',
-        bucket: 'recipes-of-marine'
-    })
-});
 
 //app.use('/public', express.static(__dirname + '/public'));
 //app.use(express('views'));
@@ -42,7 +70,3 @@ app.get('/upload', function (req, res) {
     res.render('testform')
 })
 */
-app.post('/upload', uploadHandler.any(), function (req, res) {
-    console.log("azertyuio")
-    res.json(req.files);
-});
