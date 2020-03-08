cda472a6e4a7018f771d738aa4c7648d4df70593
marineteroitin <marine.teroitin@gmail.com>
2020-03-08 10:13:09 +0100

    update for heroku

--- server/serveur.js
+++ server/serveur.js
@@ -28,11 +28,11 @@ app.use("/server", Users)
 app.use("/server", Recipes)
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/marine\'srecipe'));
 
 app.get('/*', function (req, res) {
 
-  res.sendFile(path.join(__dirname + '/dist/<name-of-app>/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/marine\'srecipe/index.html'));
 });
 
 app.listen(port, function () {
