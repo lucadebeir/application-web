09eab157f09afa67634e28b1994cb0d81e3d6705
marineteroitin <marine.teroitin@gmail.com>
2020-04-05 16:16:37 +0200

    css

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,29 @@
-const Sequelize = require("sequelize")
+/*const Sequelize = require("sequelize")
 const db = {}
 const sequelize = new Sequelize("marinesrecipes_bd", "201428", "Luka-12021996", {
     host: "mysql-marinesrecipes.alwaysdata.net",
+    dialect: "mysql",
+    operatorsAliases: false,
+
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+})
+
+db.sequelize = sequelize
+db.Sequelize = Sequelize
+
+module.exports = db*/
+
+//local
+const Sequelize = require("sequelize")
+const db = {}
+const sequelize = new Sequelize("marine'srecipes", "root", "root", {
+    host: "localhost",
+    port: 8889,
     dialect: "mysql",
     operatorsAliases: false,
 
