359916ad3c6ed4d35e7cd3e3eefb3aa660c9a965
marineteroitin <marine.teroitin@gmail.com>
2020-06-24 19:35:27 +0200

    build app

--- server/dist/marine'srecipe/polyfills-es2015.js
+++ server/dist/marine'srecipe/polyfills-es2015.js
@@ -3239,7 +3239,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/lucadebeir/Documents/GitHub/projet-web/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /Users/marineteroitin/Documents/GitHub/projet-web/src/polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
