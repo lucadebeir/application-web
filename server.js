ab5939db851d5c5879cc4d6fd830716c49b30c1f
Luca Debeir <l.debeir@me.com>
2020-12-14 02:35:24 +0100

    fix bugs

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express");
-
 const app = express();
+const path = require("path");
+const cors = require("cors");
+const sanitizer = require("express-sanitizer");
+const bodyParser = require("body-parser");
+
+app.use(sanitizer());
+app.use(bodyParser.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //Serve only the static files form the dist directory
 app.use(express.static(__dirname + "/dist/marine'srecipe"));
