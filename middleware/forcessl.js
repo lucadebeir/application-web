c720c2daf9fa9a517551c1ffe31125008baf6e55
lucadebeir <l.debeir@me.com>
2021-02-21 22:34:20 +0100

    Delete forcessl.js

--- middleware/forcessl.js
+++
@@ -1,9 +0,0 @@
-module.exports = function () {
-  return function forceSSL(req, res, next) {
-    var FORCE_HTTPS = process.env.FORCE_HTTPS || false;
-    if (req.headers["x-forwarded-proto"] !== "https" && FORCE_HTTPS) {
-      return res.redirect(["https://", req.get("Host"), req.url].join(""));
-    }
-    next();
-  };
-};
