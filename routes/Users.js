bd062f81b9894346d754dc46343c20b5aed5a730
lucadebeir <l.debeir@me.com>
2021-02-21 23:46:39 +0100

    Update Users.js

--- routes/Users.js
+++ routes/Users.js
@@ -140,7 +140,10 @@ users.get("/verify", function (req, res) {
   console.log(req.protocol);
   console.log(req.get("host"));
   console.log(host);
-  if (req.protocol + "://" + req.get("host") == "http://marine-s-recipes") {
+  if (
+    req.protocol + "://" + req.get("host") ==
+    "http://marine-s-recipes.herokuapp.com"
+  ) {
     if (req.query.id == rand) {
       User.update(
         { emailConfirmed: true },
